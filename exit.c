3998cc8c538a054241362e02c030c82a85129615
rizkifonna <rakifonna@outlook.com>
2019-11-01 17:23:16 +0700

    Penambahan command exit

---
+++ exit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "exit.h"
+
+
+
+void exitGame()
+{
+    exit(0);
+
+}
---
+++ exit.h
@@ -0,0 +1,18 @@
+#ifndef __EXIT_H__
+#define __EXIT_H__
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "boolean.h"
+#include "point.h"
+#include "typebentukan.h"
+#include "stack.h"
+
+void exitGame();
+
+
+
+
+#endif
