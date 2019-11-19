09b2de90a0219e67aec6bf413591a6a3cab03d84
Raras Pradnya Pramudita <r4r4s274@Rarass-MacBook-Pro.local>
2019-11-19 16:09:56 +0700

    graph baru cokkk

---
+++ graph_baru.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matriks.h"
+#include "boolean.h"
+#include "list.h"
+#include "graph.h"
+
+
+/****************** Manajemen Memori ******************/
+addresslistG AlokasiGraph (infotypelist X, List L){
+/* Mengirimkan addresslist hasil alokasi sebuah elemen */
+/* Jika alokasi berhasil, maka addresslist tidak nil, dan misalnya */
+/* menghasilkan P, maka Info(P)=X, Next(P)=NilList */
+/* Jika alokasi gagal, mengirimkan NilList */
+	addresslistG P;
+	P =  malloc(1*sizeof(infotypelistGraph));
+	if (P == NilList){
+		return P;
+	}
+	else{
+		Induk(P) = X;
+		Anak(P) = L;
+		NextG(P) = NilList;
+		return P;
+	}
+}
+
+/****************** Penambahan Elemen ******************/
+void InsVFirstGraph (GraphList *Graph, infotypelist X, List L){
+/* I.S. L mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen pertama dengan nilai X jika alokasi berhasil */
+	addresslistG P;
+	P = AlokasiGraph(X, L);
+
+	if (P != NilList){
+		NextG(P) = FirstG(*Graph);
+		FirstG(*Graph) = P;
+	}
+}
+
+void InsVLastGraph (GraphList *Graph, infotypelist X, List L){
+/* I.S. L mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen list di akhir: elemen terakhir yang baru */
+/* bernilai X jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
+	addresslistG Last, Current;
+	Current = AlokasiGraph(X, L);
+	if (Current != NilList){
+		Last = FirstG(*Graph);
+		if (Last != NilList){
+			while (NextG(Last) != NilList){
+				Last = NextG(Last);
+			}
+			NextG(Last) = Current;
+		}
+		else{
+			InsVFirstGraph(Graph,X,L);
+		}
+	}
+}
+
+
+/****************** Konstruktor ******************/
+List CreateListNeighbors(int n, MATRIKS *Mat){
+	List L;
+	CreateEmptyList(&L);
+	for (int j = KolMin; j <= GetLastIdxKol(*Mat); j++){
+		if (ElmtMat(*Mat, n, j) == 1){
+			InsVLast(&L,j);
+		}
+	}
+	return L;
+}
+
+void CreateEmptyGraph(GraphList *Graph, int maxel, MATRIKS *Mat)
+/* array untuk representasi graph, array berisikan list of bangunan yang terhubung */
+{
+	FirstG(*Graph) = NilList;
+	for (int i=BrsMin; i <= GetLastIdxBrs(*Mat); i++){
+		if (i == 1){
+			InsVFirstGraph(Graph, i, CreateListNeighbors(i, Mat));
+		}
+		else{
+			InsVLastGraph(Graph, i, CreateListNeighbors(i, Mat));
+		}
+	}
+}
+
+boolean IsAdjacent(GraphList *Graph, int a, int b)
+/* mengecek apakah bangunan a bersambung dengan bangunan b */
+{
+	addresslistG P;
+	addresslist Q;
+    boolean found;
+    P = FirstG(*Graph);
+    found = false;
+    while (P != NilList){
+        if (a == Induk(P)){
+            Q = First(Anak(P));
+			while ((Q != NilList) && (!found)){
+				if (b == Info(Q)){
+					found = true;
+				}
+				else {
+					Q = Next(Q);
+				}
+			}
+        }
+        else{
+            P = NextG(P);
+        }
+    }
+	if (P == NilList){
+		found = false;
+	}
+	else if (Q == NilList){
+		found = false;
+	}
+	else{
+		found = true;
+	}
+	return (found);
+}
---
+++ graph_baru.h
@@ -0,0 +1,61 @@
+/* ADT GRAPH */
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "boolean.h"
+#include "list.h"
+#include "matriks.h"
+
+
+#define NilList NULL
+
+
+typedef struct tElmtMatlistG *addresslistG;
+
+typedef struct{
+  infotypelist induk;
+  List anak;
+}infotypelistGraph;
+
+typedef struct tElmtMatlistG {
+	infotypelistGraph infoG;
+	addresslistG nextG;
+} ElmtListGraph;
+
+typedef struct {
+	addresslistG FirstG;
+} GraphList;
+
+/* Definisi list : */
+/* List kosong : First(L) = NilList */
+/* Setiap elemen dengan addresslist P dapat diacu Info(P), Next(P) */
+/* Elemen terakhir list : jika addressnya Last, maka Next(Last)=NilList */
+
+#define Info(P) (P)->info
+#define Next(P) (P)->next
+#define First(L) ((L).First)
+
+#define Induk(P) ((P)->infoG).induk
+#define Anak(P) ((P)->infoG).anak
+#define NextG(P) (P)->nextG
+#define FirstG(G) ((G).FirstG)
+
+
+/****************** Manajemen Memori ******************/
+addresslistG AlokasiGraph (infotypelist X, List L);
+
+/****************** Penambahan Elemen ******************/
+void InsVFirstGraph (GraphList *Graph, infotypelist X, List L);
+
+void InsVLastGraph (GraphList *Graph, infotypelist X, List L);
+
+/****************** Konstruktor ******************/
+List CreateListNeighbors(int n, MATRIKS *Mat);
+
+void CreateEmptyGraph(GraphList *Graph, int maxel, MATRIKS *Mat);
+
+boolean IsAdjacent(GraphList *Graph, int a, int b);
+
+
+#endif
