f1c5ad736eca90664c5c11f5ad57353082cb4e10
dwianiyulia <dwianiyulia@gmail.com>
2019-11-22 12:55:17 +0700

    ku masukin array lg buat ngetest. sm kayanya pop nya salah deh :(

---
+++ array.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "array.h"
+
+
+void CreateEmptyArray(TabBang *Arr, int maxel){
+/* I.S. T sembarang, maxel > 0 */
+/* F.S. Terbentuk tabel T kosong dengan kapasitas maxel + 1 */
+/* Proses: Inisialisasi semua elemen tabel T dengan ValUndef */
+	int i;
+	MaxElArr(*Arr) = maxel;
+	TI(*Arr) = (ElType*) malloc((maxel+1)*sizeof(ElType));
+	for (i = IdxMin;i <= MaxElArr(*Arr); i++){
+		Elmt(*Arr,i).jum = ValUndef;
+		Elmt(*Arr,i).lev = ValUndef;
+		Elmt(*Arr,i).A = ValUndef;
+		Elmt(*Arr,i).M = ValUndef;
+		Elmt(*Arr,i).P = false;
+		Elmt(*Arr,i).U = ValUndef;
+		Elmt(*Arr,i).letak.X = ValUndef;
+		Elmt(*Arr,i).letak.Y = ValUndef;
+	}
+}
+
+void DealokasiArr(TabBang *Arr){
+/* I.S. T terdefinisi; */
+/* F.S. TI(T) dikembalikan ke system, MaxElArr(T)=0; Neff(T)=0 */
+	CreateEmptyArray(Arr,0);
+	MaxElArr(*Arr) = 0;
+	free (TI(*Arr));
+}
+
+int NbElmtArr(TabBang T){
+/* Mengirimkan banyaknya elemen efektif tabel */
+/* Mengirimkan nol jika tabel kosong */
+	int i;
+	boolean found;
+	
+	if (Elmt(T,1).jum == ValUndef){
+		return 0;
+	}
+	
+	i = 1;
+	found = false;
+	while ((!found) && i <= MaxElArr(T)){
+			if (Elmt(T,i).jum == ValUndef){
+				found = true;
+			}
+			else {
+				i += 1;
+			}
+	}
+	
+	if (!found){
+		i = MaxElArr(T) + 1;
+	}
+	return (i-1);
+
+}
+
+/* *** Selektor INDEKS *** */
+IdxType GetFirstIdx(TabBang T){
+/* Prekondisi : Tabel T tidak kosong */
+/* Mengirimkan indeks elemen T pertama */
+	return (IdxMin);
+}
+
+IdxType GetLastIdx(TabBang T){
+/* Prekondisi : Tabel T tidak kosong */
+/* Mengirimkan indeks elemen T terakhir */
+	return (NbElmtArr(T));
+
+}
+
+void CopyArr (TabBang Tabin, TabBang *Tabout){
+	int i;
+	CreateEmptyArray (&*Tabout, MaxElArr(Tabin));
+	for (i = 1; i <= GetLastIdx(Tabin); i++){
+		Elmt(*Tabout,i).nomor = Elmt(Tabin,i).nomor;
+		Elmt(*Tabout,i).type = Elmt(Tabin,i).type;
+		Elmt(*Tabout,i).jum = Elmt(Tabin,i).jum;
+		Elmt(*Tabout,i).lev = Elmt(Tabin,i).lev;
+		Elmt(*Tabout,i).A = Elmt(Tabin,i).A;
+		Elmt(*Tabout,i).M = Elmt(Tabin,i).M;
+		Elmt(*Tabout,i).P = Elmt(Tabin,i).P;
+		Elmt(*Tabout,i).U = Elmt(Tabin,i).U; 
+		Elmt(*Tabout,i).letak.X = Elmt(Tabin,i).letak.X; 
+		Elmt(*Tabout,i).letak.Y = Elmt(Tabin,i).letak.Y; 
+		Elmt(*Tabout,i).attack = Elmt(Tabin,i).attack; 
+		Elmt(*Tabout,i).move = Elmt(Tabin,i).move; 
+	}
+}
--- stack_driver.c
+++ stack_driver.c
@@ -3,6 +3,7 @@
 #include "typebentukan.h"
 #include "array.h"
 #include "point.h"
+#include "boolean.h"
 
 void printBool(boolean X){
     if(X){
@@ -18,6 +19,7 @@ int main(){
     char code,inputchar;
     infotypestack inp,X,inpfpush;
     int input,N,i;
+    i=0;
     CreateEmptyStack(&S);
     CreateEmptyStack(&SI);
     CreateEmptyStack(&SC);
@@ -39,57 +41,60 @@ int main(){
             printf("Masukkan max elmt TabBang : ");
             scanf("%d", &input);
             inp.MaxElArr = input;
+            CreateEmptyArray(&inp,inp.MaxElArr);
             printf("Masukkaan array of Bangunan : \n");
             printf("Masukkaan ada berapa elemen of bangunan yang mau ditempatkan ke array : ");
             scanf("%d", &N);
-            for(i = 1; i <= N; i++){
-                printf("Masukkan no bangunan : ");
-                scanf("%d", &input);
-                Elmt(inp,i).nomor = input;
-                printf("Masukkan type bangunan : ");
-                scanf(" %c", &inputchar);
-                Elmt(inp,i).nomor = inputchar;
-                printf("Masukkan jum bangunan : ");
-                scanf("%d", &input);
-                Elmt(inp,i).jum = input;
-                printf("Masukkan lev bangunan : ");
-                scanf("%d", &input);
-                Elmt(inp,i).lev = input;
-                printf("Masukkan A bangunan : ");
-                scanf("%d", &input);
-                Elmt(inp,i).A = input;
-                printf("Masukkan M bangunan : ");
-                scanf("%d", &input);
-                Elmt(inp,i).M = input;
-                printf("Masukkan P bangunan 0/1: ");
-                scanf("%d", &input);
-                if(input == 0){
-                    Elmt(inp,i).P = false;
-                } else{
-                    Elmt(inp,i).P = true;
-                }
-                printf("Masukkan U bangunan : ");
-                scanf("%d", &input);
-                Elmt(inp,i).U = input;
-                printf("Masukkan letak bangunan x: ");
-                scanf("%d", &input);
-                Elmt(inp,i).letak.X = input;
-                printf("Masukkan letak bangunan y: ");
-                scanf("%d", &input);
-                Elmt(inp,i).letak.Y = input;
-                printf("Masukkan attack bangunan 0/1: ");
-                scanf("%d", &input);
-                if(input == 0){
-                    Elmt(inp,i).attack = false;
-                } else{
-                    Elmt(inp,i).attack = true;
-                }
-                printf("Masukkan move bangunan 0/1: ");
-                scanf("%d", &input);
-                if(input == 0){
-                    Elmt(inp,i).move = false;
-                } else{
-                    Elmt(inp,i).move = true;
+            if(N <= inp.MaxElArr){
+                for(i = 1; i <= N; i++){
+                    printf("Masukkan no bangunan : ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).nomor = input;
+                    printf("Masukkan type bangunan : ");
+                    scanf(" %c", &inputchar);
+                    Elmt(inp,i).type = inputchar;
+                    printf("Masukkan jum bangunan : ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).jum = input;
+                    printf("Masukkan lev bangunan : ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).lev = input;
+                    printf("Masukkan A bangunan : ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).A = input;
+                    printf("Masukkan M bangunan : ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).M = input;
+                    printf("Masukkan P bangunan 0/1: ");
+                    scanf("%d", &input);
+                    if(input == 0){
+                        Elmt(inp,i).P = false;
+                    } else{
+                        Elmt(inp,i).P = true;
+                    }
+                    printf("Masukkan U bangunan : ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).U = input;
+                    printf("Masukkan letak bangunan x: ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).letak.X = input;
+                    printf("Masukkan letak bangunan y: ");
+                    scanf("%d", &input);
+                    Elmt(inp,i).letak.Y = input;
+                    printf("Masukkan attack bangunan 0/1: ");
+                    scanf("%d", &input);
+                    if(input == 0){
+                        Elmt(inp,i).attack = false;
+                    } else{
+                        Elmt(inp,i).attack = true;
+                    }
+                    printf("Masukkan move bangunan 0/1: ");
+                    scanf("%d", &input);
+                    if(input == 0){
+                        Elmt(inp,i).move = false;
+                    } else{
+                        Elmt(inp,i).move = true;
+                    }
                 }
             }
             Push(&S,inp);
@@ -97,22 +102,23 @@ int main(){
             Pop(&S,&X);
         } else if(code == 'F'){
             if(IsFullStack(S)){
-                printf("Stack penuh\n");
+                printf("Stack penuh\n\n");
             } else{
-                printf("Stack tak penuh\n");
+                printf("Stack tak penuh\n\n");
             }
         } else if(code == 'E'){
             if(IsEmptyStack(S)){
-                printf("Stack kosong\n");
+                printf("Stack kosong\n\n");
             } else{
-                printf("Stack tak kosong\n");
+                printf("Stack tak kosong\n\n");
             }
         } else if(code == 'I'){
             SI = InverseStack(S);
             printf("Success to Inverse!\n");
             printf("Stack Inverse: \n");
-            while (Top(SI) != NilStack){
+            // while (Top(SI) != NilStack){
                 for(i = 1; i <= N; i++){
+                    printf("%d\n\n",i);
                     printf("No Bangunan : %d\n", Elmt(InfoTop(SI),i).nomor);
                     printf("Type Bangunan : %c\n", Elmt(InfoTop(SI),i).type);
                     printf("Jum Bangunan : %d\n", Elmt(InfoTop(SI),i).jum);
@@ -128,14 +134,14 @@ int main(){
                     printf("Move Bangunan : ");
                     printBool(Elmt(InfoTop(SI),i).move);
                 }
-                Pop(&SI,&X);
-            }
+            // }
         } else if(code == 'C'){
             SC = CopyStack(S);
             printf("Coppied!\n");
             printf("Stack Copy: \n");
-            while (Top(SC) != NilStack){
+            // while (Top(SC) != NilStack){
                 for(i = 1; i <= N; i++){
+                    printf("%d\n\n", i);
                     printf("No Bangunan : %d\n", Elmt(InfoTop(SC),i).nomor);
                     printf("Type Bangunan : %c\n", Elmt(InfoTop(SC),i).type);
                     printf("Jum Bangunan : %d\n", Elmt(InfoTop(SC),i).jum);
@@ -151,67 +157,30 @@ int main(){
                     printf("Move Bangunan : ");
                     printBool(Elmt(InfoTop(SC),i).move);
                 }
-                Pop(&SC,&X);
-            }
+            // }
         } else if(code == 'V'){
-            printf("Masukkan input X FPush : \n");
-            printf("Masukkan max elmt TabBang : ");
-            scanf("%d", &input);
-            inpfpush.MaxElArr = input;
-            printf("Masukkaan array of Bangunan : \n");
-            printf("Masukkaan ada berapa elemen of bangunan yang mau ditempatkan ke array : ");
-            scanf("%d", &N);
-            for(i = 1; i <= N; i++){
-                printf("Masukkan no bangunan : ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).nomor = input;
-                printf("Masukkan type bangunan : ");
-                scanf(" %c", &inputchar);
-                Elmt(inpfpush,i).nomor = inputchar;
-                printf("Masukkan jum bangunan : ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).jum = input;
-                printf("Masukkan lev bangunan : ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).lev = input;
-                printf("Masukkan A bangunan : ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).A = input;
-                printf("Masukkan M bangunan : ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).M = input;
-                printf("Masukkan P bangunan 0/1: ");
-                scanf("%d", &input);
-                if(input == 0){
-                    Elmt(inpfpush,i).P = false;
-                } else{
-                    Elmt(inpfpush,i).P = true;
+            SV = FPush(S,inp);
+            printf("Stack FPush : \n");
+            // while (Top(SV) != NilStack){
+                for(i = 1; i <= N; i++){
+                    printf("%d\n\n", i);
+                    printf("No Bangunan : %d\n", Elmt(InfoTop(SV),i).nomor);
+                    printf("Type Bangunan : %c\n", Elmt(InfoTop(SV),i).type);
+                    printf("Jum Bangunan : %d\n", Elmt(InfoTop(SV),i).jum);
+                    printf("Lev Bangunan : %d\n", Elmt(InfoTop(SV),i).lev);
+                    printf("A Bangunan : %d\n", Elmt(InfoTop(SV),i).A);
+                    printf("M Bangunan : %d\n", Elmt(InfoTop(SV),i).M);
+                    printf("P Bangunan : ");
+                    printBool(Elmt(InfoTop(SV),i).P);
+                    printf("U Bangunan : %d\n", Elmt(InfoTop(SV),i).U);
+                    printf("P Bangunan (x,y) : (%d,%d)\n ", Elmt(InfoTop(SV),i).letak.X,Elmt(InfoTop(SV),i).letak.Y);
+                    printf("Attack Bangunan : ");
+                    printBool(Elmt(InfoTop(SV),i).attack);
+                    printf("Move Bangunan : ");
+                    printBool(Elmt(InfoTop(SV),i).move);
                 }
-                printf("Masukkan U bangunan : ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).U = input;
-                printf("Masukkan letak bangunan x: ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).letak.X = input;
-                printf("Masukkan letak bangunan y: ");
-                scanf("%d", &input);
-                Elmt(inpfpush,i).letak.Y = input;
-                printf("Masukkan attack bangunan 0/1: ");
-                scanf("%d", &input);
-                if(input == 0){
-                    Elmt(inpfpush,i).attack = false;
-                } else{
-                    Elmt(inpfpush,i).attack = true;
-                }
-                printf("Masukkan move bangunan 0/1: ");
-                scanf("%d", &input);
-                if(input == 0){
-                    Elmt(inpfpush,i).move = false;
-                } else{
-                    Elmt(inpfpush,i).move = true;
-                }
-            }
-            SV = FPush(S,inpfpush);
+            //     Pop(&SV,&X);
+            // }
         } else if(code == 'X'){
             stop = true;
         } else{
@@ -221,6 +190,7 @@ int main(){
     printf("Stack : \n");
     while (Top(S) > NilStack){
         for(i = 1; i <= N; i++){
+            printf("%d\n\n", i);
             printf("No Bangunan : %d\n", Elmt(InfoTop(S),i).nomor);
             printf("Type Bangunan : %c\n", Elmt(InfoTop(S),i).type);
             printf("Jum Bangunan : %d\n", Elmt(InfoTop(S),i).jum);
@@ -237,27 +207,6 @@ int main(){
             printBool(Elmt(InfoTop(S),i).move);
         }
         Pop(&S,&X);
-    }
-
-    printf("Stack FPush : \n");
-    while (Top(SV) != NilStack){
-        for(i = 1; i <= N; i++){
-            printf("No Bangunan : %d\n", Elmt(InfoTop(SV),i).nomor);
-            printf("Type Bangunan : %c\n", Elmt(InfoTop(SV),i).type);
-            printf("Jum Bangunan : %d\n", Elmt(InfoTop(SV),i).jum);
-            printf("Lev Bangunan : %d\n", Elmt(InfoTop(SV),i).lev);
-            printf("A Bangunan : %d\n", Elmt(InfoTop(SV),i).A);
-            printf("M Bangunan : %d\n", Elmt(InfoTop(SV),i).M);
-            printf("P Bangunan : ");
-            printBool(Elmt(InfoTop(SV),i).P);
-            printf("U Bangunan : %d\n", Elmt(InfoTop(SV),i).U);
-            printf("P Bangunan (x,y) : (%d,%d)\n ", Elmt(InfoTop(SV),i).letak.X,Elmt(InfoTop(SV),i).letak.Y);
-            printf("Attack Bangunan : ");
-            printBool(Elmt(InfoTop(SV),i).attack);
-            printf("Move Bangunan : ");
-            printBool(Elmt(InfoTop(SV),i).move);
-        }
-        Pop(&SV,&X);
     }
     return 0;
 }
