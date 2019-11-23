b0afeea45e07acbd40287631b70bb19ee34b3f56
mfauzanalg <mfauzanalg@gmail.com>
2019-11-23 21:38:00 +0700

    oke keren

---
+++ stack/run.sh
@@ -0,0 +1,2 @@
+gcc -o stack_driver stack_driver.c ../array/array.c ../list/list.c ../matriks/matriks.c ../mesinkar/mesinkar.c ../mesinkata/mesinkata.c ../olahfile/olahfile.c ../pcolor/pcolor.c ../queue/queue.c ../stack/stack.c ../mesinchar/mesinchar.c ../mesinword/mesinword.c ../graph/graph.c ../stackplayer/stackplayer.c ../command/command.c ../save/coba_save.c 
+./stack_driver
