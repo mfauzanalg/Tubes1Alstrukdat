d492c39902817bb47375bf251908a3b6bc6b94f1
mfauzanalg <mfauzanalg@gmail.com>
2019-11-24 13:26:54 +0700

    benerin driver

--- mesinchar/mesinchar_driver.c
+++ mesinchar/mesinchar_driver.c
@@ -6,14 +6,14 @@
 
 int main()
 {
+    printf("Masukan sebuah kalimat diakhiri enter : \n");
     STARTCHAR();
+    printf("maka keluarannya adalah :\n");
     printf("%c", CC);
     while (CC!=MARK)
     {
         ADVCHAR();
         printf("%c", CC);
     }
-
-
     return 0;
 }
---
+++ mesinkar/run.sh
@@ -0,0 +1,2 @@
+gcc -o mesinkar_driver mesinkar_driver.c mesinkar.c
+./mesinkar_driver
