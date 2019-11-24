52981c468d59873d37d0853f5938a32b2cc2033a
rizkifonna <rakifonna@outlook.com>
2019-11-24 12:14:03 +0700

    Membuat driver mesinkar

---
+++ mesinkar/mmesinkar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mesinkar.h"
+
+
+
+int main()
+{
+    START();
+    printf("%c", CC);
+    while (CC!='.')
+    {
+        ADV();
+        printf("%c", CC);
+
+
+    }
+
+
+
+    return 0;
+}
